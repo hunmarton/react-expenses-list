4c4f96325229af673c39a589b8e4fcc7b83abf34
Marton Hun <hunmarton@hotmail.com>
2021-04-19 11:25:24 +0200

    working

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,29 @@
+import Card from "../UI/Card";
+import "../../components/Expenses/Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear} />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
